9c67a82b8b9f643fdac926c84117cd6110888900
brunopeople <brunopessoa.melo@gmail.com>
2018-06-03 17:42:04 -0300

    subindo projeto, criando os componentes

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core'
+import{ Component } from '@angular/core'
 
 @Component({
-    selector: 'app-topo',
-    templateUrl: './topo.component.html',
-    styleUrls:['./topo.component.css']
+	selector: 'app-topo',
+	templateUrl:'./topo.component.html',
+	styleUrls:['./topo.component.css']
+	
 })
-export class TopoComponent {}
+export class TopoComponent{
+
+}
